a4dbdf116b630575fa605316607866ebf12d747c
mandujano.cruz <jesus.mandujano.cruz@outlook.com>
2024-11-26 13:11:17 -0600

    Añade detalles del funcionamiento del modal

--- scripts/script.js
+++ scripts/script.js
@@ -2,20 +2,21 @@ let formElement = document.querySelector(".popup__container");
 let openPopupButton = document.querySelector(".profile__edit-button");
 let closePopupButton = document.querySelector(".popup__close");
 let popup = document.querySelector(".popup");
+let nameInput = document.querySelector(".popup__input_name");
+let jobInput = document.querySelector(".popup__input_about");
+let nameProfile=document.querySelector(".profile__name");
+let jobProfile=document.querySelector(".profile__about-me");
 
 function handleProfileFormSubmit(evt) {
     evt.preventDefault();
-    let nameInput = document.querySelector(".popup__input_name");
-    let jobInput = document.querySelector(".popup__input_about");
 
     nameInput.textContent=nameInput.value;
     jobInput.textContent=jobInput.value;
 
-    let nameProfile=document.querySelector(".profile__name");
-    let jobProfile=document.querySelector(".profile__about-me");
-
     nameProfile.textContent=nameInput.value;
     jobProfile.textContent=jobInput.value;
+
+    popup.classList.add("popup_opened");
 }
 
 function closePopupEdit() {
@@ -24,6 +25,8 @@ function closePopupEdit() {
 
 function openPopupEdit(){
     popup.classList.remove("popup_opened");
+    nameInput.value=nameProfile.textContent;
+    jobInput.value=jobProfile.textContent;
 }
 
 formElement.addEventListener('submit', handleProfileFormSubmit);
