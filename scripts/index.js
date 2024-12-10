ab87a401473261dad94fa60bba36a5c849e3f85d
mandujano.cruz <jesus.mandujano.cruz@outlook.com>
2024-12-10 14:59:00 -0600

    Añade template y recopila datos desde js

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,45 @@
 const initialCards = [
     {
         name: "Boston",
-        link: "https://images.unsplash.com/photo-1531874668635-85a25b7279a2?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        link: "https://images.unsplash.com/photo-1531874668635-85a25b7279a2?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "Fotografía de la calle Arcon en Boston"
     },
     {
         name: "Hollywood",
-        link: "https://images.unsplash.com/photo-1568502303729-8d32d9dd97a6?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        link: "https://images.unsplash.com/photo-1568502303729-8d32d9dd97a6?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "Fotografía de una de las calles de Hollywood"
     },
     {
         name: "Valle de los Monumentos",
-        link: "https://images.unsplash.com/photo-1558353016-37b89cf18040?q=80&w=1031&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        link: "https://images.unsplash.com/photo-1558353016-37b89cf18040?q=80&w=1031&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "Fotografía del Valle de los monumentos"
     },
     {
         name: "Stafford TX",
-        link: "https://images.unsplash.com/photo-1625110180367-45169a188533?q=80&w=1050&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        link: "https://images.unsplash.com/photo-1625110180367-45169a188533?q=80&w=1050&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "Fotografía exterior de Stafford, Texas"
     },
     {
         name: "Estatua de la Libertad",
-        link: "https://images.unsplash.com/photo-1520885161078-041b9f6ad3ef?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        link: "https://images.unsplash.com/photo-1520885161078-041b9f6ad3ef?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "Fotografía de la Estatua de a Libertad"
     },
     {
         name: "Capitolio",
-        link: "https://images.unsplash.com/photo-1648135589381-a07e0ca19d87?q=80&w=985&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        link: "https://images.unsplash.com/photo-1648135589381-a07e0ca19d87?q=80&w=985&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "Fotografía del Capitolio en Washington"
     }
 ];
 
-let formElement = document.querySelector(".popup__container");
-let openPopupButton = document.querySelector(".profile__edit-button");
-let closePopupButton = document.querySelector(".popup__close");
-let popup = document.querySelector(".popup");
-let nameInput = document.querySelector(".popup__input_name");
-let jobInput = document.querySelector(".popup__input_about");
-let nameProfile=document.querySelector(".profile__name");
-let jobProfile=document.querySelector(".profile__about-me");
+const formElement = document.querySelector(".popup__container");
+const openPopupButton = document.querySelector(".profile__edit-button");
+const closePopupButton = document.querySelector(".popup__close");
+const popup = document.querySelector(".popup");
+const nameInput = document.querySelector(".popup__input_name");
+const jobInput = document.querySelector(".popup__input_about");
+const nameProfile=document.querySelector(".profile__name");
+const jobProfile=document.querySelector(".profile__about-me");
+const sectionElements = document.querySelector(".elements");
 
 function handleProfileFormSubmit(evt) {
     evt.preventDefault();
@@ -55,6 +62,17 @@ function openPopupEdit(){
     nameInput.value=nameProfile.textContent;
     jobInput.value=jobProfile.textContent;
 }
+
+initialCards.forEach(function (card) {
+    const cardTemplate = document.querySelector("#card-template").content;
+    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+    cardElement.querySelector(".card__title").textContent = card.name;
+    cardElement.querySelector(".card__image").setAttribute("src", card.link);
+    cardElement.querySelector(".card__image").setAttribute("alt", card.alt);
+
+    sectionElements.append(cardElement);
+});
 
 formElement.addEventListener('submit', handleProfileFormSubmit);
 closePopupButton.addEventListener('click', closePopupEdit);
