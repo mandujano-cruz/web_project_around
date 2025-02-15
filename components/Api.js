38d2238bf82e514f8110d21d5844fe291b5780e4
mandujano_cruz <jesus.mandujano.cruz@outlook.com>
2025-02-15 00:35:45 -0600

    feat: se añade metodos a api

--- components/Api.js
+++ components/Api.js
@@ -6,7 +6,8 @@ class Api {
   getInitialCards () {
     return fetch("https://around-api.es.tripleten-services.com/v1/cards", {
       headers: {
-        authorization: "f79f57e0-6adb-472c-835e-8925770b15f2"
+        authorization: "f79f57e0-6adb-472c-835e-8925770b15f2",
+        "Content-Type": "application/json"
       }
     })
       .then(res => {
@@ -16,12 +17,69 @@ class Api {
         return Promise.reject(`Error: ${res.status}`);
       });
   }
+
+  getUserInfo () {
+    return fetch("https://around-api.es.tripleten-services.com/v1/users/me", {
+      headers: {
+        authorization: "f79f57e0-6adb-472c-835e-8925770b15f2",
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        if(res.ok){
+          return res.json();
+        }
+        return Promise.reject(`Error: ${res.status}`);
+      });
+  }
+
+  setProfile (name, about) {
+    return fetch("https://around-api.es.tripleten-services.com/v1/users/me", {
+      method: "PATCH",
+      headers: {
+        authorization: "f79f57e0-6adb-472c-835e-8925770b15f2",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: name,
+        about: about
+      })
+    })
+      .then(res => {
+        if(res.ok){
+          return res.json();
+        }
+        return Promise.reject(`Error: ${res.status}`);
+      });
+  }
+
+  addCard (name, link) {
+    return fetch("https://around-api.es.tripleten-services.com/v1/cards", {
+      method: "POST",
+      headers: {
+        authorization: "f79f57e0-6adb-472c-835e-8925770b15f2",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: name,
+        link: link
+      })
+    })
+      .then(res => {
+        if(res.ok){
+          return res.json();
+        }
+        return Promise.reject(`Error: ${res.status}`);
+      });
+  }
+
+
 }
 
 const api = new Api({
   baseUrl: "https://around-api.es.tripleten-services.com/v1",
   headers: {
     authorization: "f79f57e0-6adb-472c-835e-8925770b15f2",
-    "Content-Type": "aplication/json"
+    "Content-Type": "application/json"
   }
 });
