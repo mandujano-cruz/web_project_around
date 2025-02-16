f23d85d4f0da4df93694ad7a863a3cc842e386d9
mandujano_cruz <jesus.mandujano.cruz@outlook.com>
2025-02-16 01:03:23 -0600

    refactor: se eliminan los comentarios

--- components/PopupWithConfirmation.js
+++ components/PopupWithConfirmation.js
@@ -10,12 +10,6 @@ export default class PopupWithConfirmation extends Popup {
     this._handleConfirm = action;
   }
 
-  // open (cardElement, cardId) {
-  //   this._cardElement = cardElement;
-  //   this._cardId = cardId;
-  //   super.open();
-  // }
-
   setEventListeners () {
     super.setEventListeners();
     this._form.addEventListener("submit", (evt) => {
