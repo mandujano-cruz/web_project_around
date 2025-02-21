57165fe4b57061fdb4f548b71e1339d6612f6d5a
mandujano.cruz <jesus.mandujano.cruz@outlook.com>
2025-02-21 15:49:55 -0600

    refactor: se eliminan constantes sin utilizar

--- utils/constants.js
+++ utils/constants.js
@@ -6,46 +6,10 @@ export const config = {
     errorClass: 'popup__input-error_active'
 };
 
-export const initialCards = [
-    {
-        name: "Boston",
-        link: "https://images.unsplash.com/photo-1531874668635-85a25b7279a2?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    },
-    {
-        name: "Hollywood",
-        link: "https://images.unsplash.com/photo-1568502303729-8d32d9dd97a6?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    },
-    {
-        name: "Valle de los Monumentos",
-        link: "https://images.unsplash.com/photo-1558353016-37b89cf18040?q=80&w=1031&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    },
-    {
-        name: "Stafford TX",
-        link: "https://images.unsplash.com/photo-1625110180367-45169a188533?q=80&w=1050&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    },
-    {
-        name: "Estatua de la Libertad",
-        link: "https://images.unsplash.com/photo-1520885161078-041b9f6ad3ef?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    },
-    {
-        name: "Capitolio",
-        link: "https://images.unsplash.com/photo-1648135589381-a07e0ca19d87?q=80&w=985&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    }
-];
 
-export const popupAdd = document.querySelector(".popup_add");
-export const popupEdit = document.querySelector(".popup_edit");
 export const cardList = document.querySelector(".elements");
 export const nameInput = document.querySelector(".popup__input-name");
 export const jobInput = document.querySelector(".popup__input-about");
-export const nameProfile = document.querySelector(".profile__name");
-export const jobProfile = document.querySelector(".profile__about-me");
-export const overlay = document.querySelector(".overlay");
-export const popupImage = document.querySelector(".popup__image");
-export const popupImageTitle = document.querySelector(".popup__image-title");
-export const popupCloseButton = document.querySelector(".popup__close");
 export const editButton = document.querySelector(".profile__edit-button");
 export const addButton = document.querySelector(".profile__add-image");
-
-export const profilePhoto = document.querySelector(".profile__image");
 export const containerPhoto = document.querySelector(".profile__container");
